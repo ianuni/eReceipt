b21299b92ba369a543c84fc614ba9f097b52f51c
JavierToron <javitoron@student.agh.edu.pl>
2022-12-06 00:08:33 +0000

    Finished changing the icon and creating the splash of the app

--- app/src/main/java/com/example/ereceipt/MainActivity.kt
+++ app/src/main/java/com/example/ereceipt/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private val fireViewModel : FirebaseViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_EReceipt)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         fireViewModel.setdata(FirebaseImplementation())
